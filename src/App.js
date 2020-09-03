6ec58adb730ca23e232f2771b4b5a86609e25643
Haoyu <hliu687@gatech.edu>
2020-09-03 21:15:14 +0800

    first update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+//axios request to API
+import { fetchData } from './api'
+//====Import Component=====
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//you need to configure index.js in components folder
+import { Cards, Chart, CountryPicker } from './components';
+
+class App extends React.Component {
+
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+    }
+    render() {
+        //解构赋值
+        const { data } = this.state;
+
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                {/* <CountryPicker /> */}
+                <br></br>
+                <h3>What do you think 😭</h3>
+                <Chart data={data}/>
+                
+            </div>
+        )
+    }
 }
 
 export default App;
